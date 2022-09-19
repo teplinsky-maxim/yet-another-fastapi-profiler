b0c98448140483fa5339b467cd24b517a62c303d
Maxim Teplinsky <there_is_no_email@yep.com>
2022-09-19 11:53:50 +0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='yet_another_fastapi_profiler',
@@ -6,8 +6,8 @@ setup(
     license='MIT',
     author="Maxim Teplinsky",
     author_email='email@example.com',
-    packages=find_packages('yet_another_fastapi_profiler'),
-    package_dir={'': 'yet_another_fastapi_profiler'},
+    packages=['yet_another_fastapi_profiler'],
+    package_dir={'yet_another_fastapi_profiler': 'yet_another_fastapi_profiler'},
     url='https://github.com/teplinsky-maxim/yet-another-fastapi-profiler',
     keywords='fastapi yet_another_fastapi_profiler filter minimal time',
     install_requires=[
